240d20b42c6ac332a48fbf55d41823c41ac0164a
Martin Di Paola <martinp.dipaola@gmail.com>
2023-06-18 10:12:20 -0300

    test: support partial hexdump on const fp
    Instead of playing with fp's get pointers (seekg), pass a const
    reference to avoid any modification.
    
    Then, pass the optional at and len parameters to run hexdump on
    the byte <at> and <len> bytes following.
    
    By default <at> is 0 and <len> is -1 which means hexdump the entire fp.

--- test/testing_xoz.cpp
+++ test/testing_xoz.cpp
@@ -1,18 +1,23 @@
 #include "test/testing_xoz.h"
 #include <iomanip>
 
-std::string testing_xoz::helpers::hexdump(std::stringstream& fp) {
-    auto cur = fp.tellg();
+std::string testing_xoz::helpers::hexdump(const std::stringstream& fp, unsigned at, unsigned len) {
+    std::ostringstream out;
 
-    std::stringstream out;
+    // Handle the case that we overflow the unsigned integer
+    // Avoid the wrap around
+    if (at + len < at) {
+        len = unsigned(-1) - at;
+    }
 
+    // fp *must* be a const reference so calling str() does
+    // not change its internal get pointer (seekg)
     std::string bytes = fp.str();
-    for (unsigned i = 0; i < bytes.size(); ++i) {
+    for (unsigned i = at; i < bytes.size() and i < at + len; ++i) {
         out << std::setfill('0') << std::setw(2) << std::hex << (int)(unsigned char)bytes[i];
-        if (i % 2 == 1 and i+1 < bytes.size())
+        if (i % 2 == 1 and i+1 < bytes.size() and i+1 < at + len)
             out << " ";
     }
 
-    fp.seekg(cur);
     return out.str();
 }
