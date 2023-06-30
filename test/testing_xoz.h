5b4dd03b115628f95c0788ae6fcac6702313ba42
Martin Di Paola <martinp.dipaola@gmail.com>
2023-06-29 21:13:20 -0300

    test: workaround for EXPECT_THAT on throws that call func twice
    EXPECT_THAT, when a func under test throws an exception, it calls the
    func again (twice) which has non-trivial side effects.
    
    This is mostly likely a bug in GoogleTest.
    
    The workaround wraps the func under test and ensure that it is called
    once: on the second call, it just rethrow the exception.

--- test/testing_xoz.h
+++ test/testing_xoz.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <functional>
+#include <memory>
 
 namespace testing_xoz {
     namespace helpers {
@@ -33,6 +35,30 @@ namespace testing_xoz {
         }
 
         const std::stringstream file2mem(const char* path);
+
+        // Credit:
+        // https://github.com/google/googletest/issues/4073#issuecomment-1384645201
+        template<class Function>
+        std::function<void()> ensure_called_once(Function function)
+        {
+            auto shared_exception_ptr = std::make_shared<std::exception_ptr>();
+            auto was_called = std::make_shared<bool>(false);
+            return [shared_exception_ptr, was_called, function]() {
+                if (*shared_exception_ptr) {
+                    std::rethrow_exception(*shared_exception_ptr);
+                }
+                if (*was_called) {
+                    return;
+                }
+                *was_called = true;
+                try {
+                    std::invoke(function);
+                } catch (...) {
+                    *shared_exception_ptr = std::current_exception();
+                    std::rethrow_exception(*shared_exception_ptr);
+                }
+            };
+        }
     }
 
     void zbreak();
