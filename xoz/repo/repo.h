e82829ab11d4dd9a3a3675e230b8a281ae84f20f
Martin Di Paola <martinp.dipaola@gmail.com>
2023-07-25 08:36:40 -0300

    repo: expose blk and subblk sizes

--- xoz/repo/repo.h
+++ xoz/repo/repo.h
@@ -121,6 +121,14 @@ public:
 
     inline const GlobalParameters& params() const { return gp; }
 
+    inline uint32_t subblk_sz() const {
+        return gp.blk_sz >> Extent::SUBBLK_SIZE_ORDER;
+    }
+
+    inline uint32_t blk_sz() const {
+        return gp.blk_sz;
+    }
+
     // Main primitive to allocate / free blocks
     //
     // This expands/shrinks the underlying physical file.
