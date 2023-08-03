e6e3381bbf9d78db4cafb62cfb0c7d3b3f73ac94
Martin Di Paola <martinp.dipaola@gmail.com>
2023-08-03 00:02:07 -0300

    alloc: refactor code, SegmentAllocator in its own module file

--- xoz/alloc/segm_allocator.h
+++ xoz/alloc/segm_allocator.h
@@ -1,10 +1,6 @@
 #pragma once
 
-#include <algorithm>
-#include <cassert>
 #include <cstdint>
-#include <cstring>
-#include <list>
 
 #include "xoz/alloc/free_map.h"
 #include "xoz/alloc/subblock_free_map.h"
@@ -44,24 +40,7 @@ private:
     uint64_t in_use_ext_per_segm[StatsExtPerSegmLen];
 
 public:
-    explicit SegmentAllocator(Repository& repo, bool coalescing_enabled = true, uint16_t split_above_threshold = 0):
-            repo(repo),
-            tail(repo),
-            fr_map(coalescing_enabled, split_above_threshold),
-            subfr_map(),
-            coalescing_enabled(coalescing_enabled),
-            in_use_by_user_sz(0),
-            in_use_blk_cnt(0),
-            in_use_blk_for_suballoc_cnt(0),
-            in_use_subblk_cnt(0),
-            in_use_ext_cnt(0),
-            in_use_inlined_sz(0),
-            alloc_call_cnt(0),
-            dealloc_call_cnt(0),
-            all_user_sz(0),
-            all_req_sz(0) {
-        memset(in_use_ext_per_segm, 0, sizeof(in_use_ext_per_segm));
-    }
+    explicit SegmentAllocator(Repository& repo, bool coalescing_enabled = true, uint16_t split_above_threshold = 0);
 
     struct req_t {
         uint16_t segm_frag_threshold;
@@ -71,165 +50,10 @@ public:
 
     constexpr static struct req_t DefaultReq = {.segm_frag_threshold = 2, .max_inline_sz = 8, .allow_suballoc = true};
 
-    Segment alloc(const uint32_t sz, const struct req_t& req = SegmentAllocator::DefaultReq) {
-        //
-        //   [------------------------------------------------------] <-- sz
-        //   :                                                      :
-        //   :      blk                  blk             blk        :
-        //   |----|----|----|----||----|----|----|----||----|----|..:
-        //   \___________________/\___________________/\________/:  :
-        //      extent (full)            (full)        (not full):  :
-        //                                                       :  :
-        //        |                      _______________________/   |
-        //        |                     /                           |
-        //        |                    [............................] <-- sz % blk_sz
-        //        |                    :                            :
-        //        |                    : subblk                     :
-        //        |                    |-------|------|-----|.......:
-        //        |                    \____________________/       :
-        //        |                        single extent    :       :
-        //        |   /----------------- for suballocation  :       :
-        //        V   V                                     :       :
-        //   +===+===+===+--------+                         :       :
-        //   |  extents  | inline |      __________________/        |
-        //   +===+===+===+--------+     /                           |
-        //          Segment    ^       [............................] <-- (sz % blk_sz) % subblk_sz
-        //                     |       :                            :
-        //                     \       :                            :
-        //                      \      |----------------------------|
-        //                       \            inline data
-        //                        \----------- /
+    Segment alloc(const uint32_t sz, const struct req_t& req = SegmentAllocator::DefaultReq);
+    void dealloc(const Segment& segm);
 
-        Segment segm;
-        uint32_t sz_remain = sz;
-        uint32_t avail_sz = 0;
-
-        // How many blocks are needed?
-        uint32_t blk_cnt_remain = sz_remain / repo.blk_sz();
-        sz_remain = sz_remain % repo.blk_sz();
-
-        // How many sub blocks are needed?
-        uint32_t subblk_cnt_remain;
-        if (req.allow_suballoc) {
-            subblk_cnt_remain = sz_remain / repo.subblk_sz();
-            sz_remain = sz_remain % repo.subblk_sz();
-        } else {
-            subblk_cnt_remain = 0;
-        }
-
-        // How many bytes are going to be inline'd?
-        uint32_t inline_sz = sz_remain;
-        sz_remain = 0;
-
-        // Backpressure: if inline sz is greater than the limit,
-        // put it into its own subblock
-        if (inline_sz > req.max_inline_sz) {
-            if (req.allow_suballoc) {
-                assert(inline_sz <= repo.subblk_sz());
-                ++subblk_cnt_remain;
-            } else {
-                assert(inline_sz <= repo.blk_sz());
-                ++blk_cnt_remain;
-            }
-            inline_sz = 0;
-        }
-
-        // Backpressure: if subblk count can fill an entire block
-        // do it
-        if (subblk_cnt_remain == Extent::SUBBLK_CNT_PER_BLK) {
-            ++blk_cnt_remain;
-            subblk_cnt_remain = 0;
-        }
-
-        // Allocate extents trying to not expand the repository
-        // but instead reusing free space already present even if
-        // that means to fragment the segment a little more
-        if (blk_cnt_remain) {
-            blk_cnt_remain = allocate_extents(segm, blk_cnt_remain, req.segm_frag_threshold, false, false);
-        }
-
-        // If we still require to allocate more blocks, just allow
-        // to expand the repository to get more free space
-        if (blk_cnt_remain) {
-            blk_cnt_remain = allocate_extents(segm, blk_cnt_remain, req.segm_frag_threshold, true, true);
-        }
-
-        if (blk_cnt_remain) {
-            goto no_free_space;
-        }
-
-        if (subblk_cnt_remain) {
-            // RFC says 16 subblocks per block only and the code above should
-            // ensure that we are dealing with one block at most.
-            assert(subblk_cnt_remain < 256);
-            assert(subblk_cnt_remain <= Extent::SUBBLK_CNT_PER_BLK);
-            subblk_cnt_remain = allocate_subblk_extent(segm, uint8_t(subblk_cnt_remain));
-        }
-
-        if (subblk_cnt_remain) {
-            goto no_free_space;
-        }
-
-        if (inline_sz) {
-            // This should be guaranteed because MaxInlineSize is a uint8_t
-            assert(inline_sz < 256);
-            segm.reserve_inline_data(uint8_t(inline_sz));
-            inline_sz = 0;
-        }
-
-        assert(blk_cnt_remain == 0);
-        assert(subblk_cnt_remain == 0);
-        assert(inline_sz == 0);
-        assert(sz_remain == 0);
-
-        avail_sz = segm.calc_usable_space_size(repo.blk_sz_order());
-        in_use_by_user_sz += avail_sz;
-        in_use_ext_cnt += segm.ext_cnt();
-        in_use_inlined_sz += segm.inline_data_sz();
-        in_use_blk_cnt += segm.blk_cnt();
-        in_use_subblk_cnt += segm.subblk_cnt();
-
-        calc_ext_per_segm_stats(segm, true);
-
-        all_user_sz += avail_sz;
-        all_req_sz += sz;
-
-        ++alloc_call_cnt;
-        return segm;
-    no_free_space:
-        throw "no free space";
-    }
-
-    void dealloc(const Segment& segm) {
-        auto sz = segm.calc_usable_space_size(repo.blk_sz_order());
-        auto blk_cnt = 0;
-        auto subblk_cnt = 0;
-        for (auto const& ext: segm.exts()) {
-            if (ext.is_suballoc()) {
-                subfr_map.dealloc(ext);
-                subblk_cnt += ext.subblk_cnt();
-            } else {
-                fr_map.dealloc(ext);
-                blk_cnt += ext.blk_cnt();
-            }
-        }
-
-        in_use_by_user_sz -= sz;
-        in_use_blk_cnt -= blk_cnt;
-        in_use_subblk_cnt -= subblk_cnt;
-        in_use_ext_cnt -= segm.ext_cnt();
-        in_use_inlined_sz -= segm.inline_data_sz();
-
-        calc_ext_per_segm_stats(segm, false);
-        ++dealloc_call_cnt;
-
-        reclaim_free_space_from_subfr_map();
-    }
-
-    void release() {
-        reclaim_free_space_from_subfr_map();
-        reclaim_free_space_from_fr_map();
-    }
+    void release();
 
     struct stats_t {
         // How many bytes are currently used (aka allocated / non-free)?
@@ -309,37 +133,7 @@ public:
         uint64_t in_use_ext_per_segm[StatsExtPerSegmLen];
     };
 
-    struct stats_t stats() const {
-        uint64_t external_frag_sz = (repo.data_blk_cnt() - in_use_blk_cnt) << repo.blk_sz_order();
-
-        uint64_t internal_frag_sz = all_user_sz - all_req_sz;
-
-        uint64_t allocable_internal_frag_sz =
-                ((in_use_blk_for_suballoc_cnt << repo.blk_sz_order()) -
-                 ((in_use_subblk_cnt << (repo.blk_sz_order() - Extent::SUBBLK_SIZE_ORDER))));
-
-        struct stats_t st = {.in_use_by_user_sz = in_use_by_user_sz,
-                             .in_use_blk_cnt = in_use_blk_cnt,
-                             .in_use_blk_for_suballoc_cnt = in_use_blk_for_suballoc_cnt,
-                             .in_use_subblk_cnt = in_use_subblk_cnt,
-
-                             .in_use_ext_cnt = in_use_ext_cnt,
-                             .in_use_inlined_sz = in_use_inlined_sz,
-
-                             .alloc_call_cnt = alloc_call_cnt,
-                             .dealloc_call_cnt = dealloc_call_cnt,
-
-                             .external_frag_sz = external_frag_sz,
-                             .internal_frag_sz = internal_frag_sz,
-
-                             .allocable_internal_frag_sz = allocable_internal_frag_sz,
-
-                             .in_use_ext_per_segm = {0}};
-
-        memcpy(st.in_use_ext_per_segm, in_use_ext_per_segm, sizeof(st.in_use_ext_per_segm));
-
-        return st;
-    }
+    struct stats_t stats() const;
 
 
     typedef xoz::alloc::internals::ConstExtentMergeIterator<FreeMap::const_iterator_by_blk_nr_t,
@@ -358,169 +152,15 @@ public:
 
 private:
     uint32_t allocate_extents(Segment& segm, uint32_t blk_cnt_remain, uint16_t segm_frag_threshold,
-                              bool ignore_segm_frag_threshold, bool use_parent) {
-        uint32_t current_segm_frag = segm.ext_cnt() <= 1 ? 0 : (segm.ext_cnt() - 1);
+                              bool ignore_segm_frag_threshold, bool use_parent);
 
-        bool frag_level_ok = (current_segm_frag < segm_frag_threshold) or ignore_segm_frag_threshold;
+    uint8_t allocate_subblk_extent(Segment& segm, uint8_t subblk_cnt_remain);
 
-        // Block count "probe" or "try" to allocate
-        uint32_t blk_cnt_probe = (uint16_t)(-1);
+    bool provide_more_space_to_fr_map(uint16_t blk_cnt);
+    bool provide_more_space_to_subfr_map();
 
-        while (blk_cnt_remain and frag_level_ok) {
-            // ensure we are not trying to allocate more blocks than can fit
-            // in a single Extent or that the ones required to hold sz data
-            blk_cnt_probe = std::min(blk_cnt_probe, blk_cnt_remain);
-            blk_cnt_probe = std::min(blk_cnt_probe, Extent::MAX_BLK_CNT);
+    void reclaim_free_space_from_fr_map();
+    void reclaim_free_space_from_subfr_map();
 
-            // Note: cast to uint16_t is OK as blk_cnt_probe is necessary smaller
-            // than MAX UINT16 because it is smaller than Extent::MAX_BLK_CNT;
-            auto result = fr_map.alloc(uint16_t(blk_cnt_probe));
-            if (result.success) {
-                assert(blk_cnt_probe == result.ext.blk_cnt());
-
-                segm.add_extent(result.ext);
-                ++current_segm_frag;
-
-                blk_cnt_remain -= result.ext.blk_cnt();
-
-            } else {
-                if (use_parent) {
-                    auto ok = provide_more_space_to_fr_map(uint16_t(blk_cnt_probe));
-                    if (not ok) {
-                        // not enough free space in parent allocator
-                        return blk_cnt_remain;
-                    }
-
-                } else {
-                    uint16_t closest_free_blk_cnt = result.ext.blk_cnt();
-
-                    if (closest_free_blk_cnt == 0) {
-                        // There is no free space, return how many blocks
-                        // are still to be allocated.
-                        return blk_cnt_remain;
-                    }
-
-                    // Try to allocate this new (smaller) block count per extent
-                    // from now and on
-                    blk_cnt_probe = closest_free_blk_cnt;
-                    ++current_segm_frag;
-                }
-            }
-
-            frag_level_ok = (current_segm_frag < segm_frag_threshold) or ignore_segm_frag_threshold;
-        }
-
-        return blk_cnt_remain;
-    }
-
-    uint8_t allocate_subblk_extent(Segment& segm, uint8_t subblk_cnt_remain) {
-        bool ok = false;
-
-    try_subfr_map_alloc:
-        auto result = subfr_map.alloc(subblk_cnt_remain);
-        if (result.success) {
-            segm.add_extent(result.ext);
-            return 0;
-        }
-
-    try_fr_map_alloc:
-        ok = provide_more_space_to_subfr_map();
-        if (ok) {
-            goto try_subfr_map_alloc;
-        }
-
-        ok = provide_more_space_to_fr_map(1);
-        if (ok) {
-            goto try_fr_map_alloc;
-        }
-
-        return subblk_cnt_remain;
-    }
-
-    bool provide_more_space_to_fr_map(uint16_t blk_cnt) {
-        if (coalescing_enabled) {
-            auto last_free_it = fr_map.crbegin_by_blk_nr();
-            if (last_free_it != fr_map.crend_by_blk_nr() and tail.is_at_the_end(*last_free_it)) {
-                auto extendable_cnt = last_free_it->blk_cnt();
-                blk_cnt = (blk_cnt <= extendable_cnt) ? 0 : (blk_cnt - extendable_cnt);
-            }
-        }
-
-        auto result = tail.alloc(blk_cnt);
-        if (result.success) {
-            fr_map.provide(result.ext);
-            return true;
-        }
-
-        return false;
-    }
-
-    bool provide_more_space_to_subfr_map() {
-        auto result = fr_map.alloc(1);
-        if (result.success) {
-            subfr_map.provide(result.ext);
-            in_use_blk_for_suballoc_cnt += result.ext.blk_cnt();
-            in_use_blk_cnt += result.ext.blk_cnt();
-            return true;
-        }
-
-        return false;
-    }
-
-    void reclaim_free_space_from_fr_map() {
-        std::list<Extent> reclaimed;
-
-        for (auto it = fr_map.crbegin_by_blk_nr(); it != fr_map.crend_by_blk_nr(); ++it) {
-            bool ok = tail.dealloc(*it);
-            if (ok) {
-                reclaimed.push_back(*it);
-            } else {
-                break;
-            }
-        }
-
-        fr_map.release(reclaimed);
-    }
-
-    void reclaim_free_space_from_subfr_map() {
-        std::list<Extent> reclaimed;
-        uint32_t blk_cnt = 0;
-
-        for (auto it = subfr_map.cbegin_full_blk(); it != subfr_map.cend_full_blk(); ++it) {
-            const auto ext = it->as_not_suballoc();
-            fr_map.dealloc(ext);
-            reclaimed.push_back(ext);
-            blk_cnt += ext.blk_cnt();
-        }
-
-        subfr_map.release(reclaimed);
-        in_use_blk_for_suballoc_cnt -= blk_cnt;
-        in_use_blk_cnt -= blk_cnt;
-    }
-
-    void calc_ext_per_segm_stats(const Segment& segm, bool is_alloc) {
-        auto ext_cnt = segm.ext_cnt();
-
-        // Ext count: 0 1 2 3 4
-        auto index = ext_cnt;
-        if (ext_cnt > 4) {
-            // Ext count: [5 8] (8 16] (16 inf)
-            index = 4;
-            if (ext_cnt <= 8) {
-                index += 1;
-            } else if (ext_cnt <= 16) {
-                index += 2;
-            } else {
-                index += 3;
-            }
-        }
-
-        assert(StatsExtPerSegmLen == 8);  // cppcheck-suppress knownConditionTrueFalse
-
-        if (is_alloc) {
-            ++in_use_ext_per_segm[index];
-        } else {
-            --in_use_ext_per_segm[index];
-        }
-    }
+    void calc_ext_per_segm_stats(const Segment& segm, bool is_alloc);
 };
