53a3cd53798fa7bb81c132219fca149215194cfa
Martin Di Paola <martinp.dipaola@gmail.com>
2023-09-09 19:30:59 -0300

    ext,segm: mv calcsz into extent.cpp and segment.cpp

--- xoz/ext/extent.cpp
+++ xoz/ext/extent.cpp
@@ -2,6 +2,10 @@
 
 #include <bitset>
 #include <iomanip>
+#include <numeric>
+
+#include "xoz/arch.h"
+#include "xoz/exceptions.h"
 
 void PrintTo(const Extent& ext, std::ostream* out) {
     std::ios_base::fmtflags ioflags = out->flags();
@@ -44,4 +48,75 @@ void PrintTo(const Extent& ext, std::ostream* out) {
 std::ostream& operator<<(std::ostream& out, const Extent& ext) {
     PrintTo(ext, &out);
     return out;
+}
+
+// Calculates the distance in blocks between a reference extent
+// and the target extent taking into account the length of each
+// extent.
+//
+// A forward distance is the count of blocks between the end of the
+// reference extent and the begin of the target extent.
+//
+// A backward distance is the count of blocks between the end of the
+// target extent and the begin of the reference extent.
+//
+// Both a forward and a backward distances are unsigned numbers
+// (0 is a valid value).
+//
+// The method returns either a forward or a backward distance:
+//  - if the target extent is *after* the reference extent, a forward
+//    distance is returned.
+//  - if the target extent is *before* the reference extent, a backward
+//    distance is returned.
+//
+// If the reference and target extents overlap, an error is raised.
+Extent::blk_distance_t Extent::distance_in_blks(const Extent& ref, const Extent& target) {
+    const uint16_t ref_blk_cnt = ref.is_suballoc() ? 1 : ref.blk_cnt();
+    const uint16_t target_blk_cnt = target.is_suballoc() ? 1 : target.blk_cnt();
+
+    uint32_t blk_cnt = 0;
+    bool is_backwards = false;
+
+    if (ref.blk_nr() < target.blk_nr()) {
+        // The current extent is *after* the reference extent
+        uint32_t forward_dist = target.blk_nr() - ref.blk_nr();
+        if (forward_dist < ref_blk_cnt) {
+            throw ExtentOverlapError(ref, target, "(ext start is ahead ref)");
+        }
+
+        blk_cnt = forward_dist - ref_blk_cnt;
+        is_backwards = false;
+
+    } else if (target.blk_nr() < ref.blk_nr()) {
+        // The current extent is *before* the reference extent
+        uint32_t backward_dist = ref.blk_nr() - target.blk_nr();
+        if (backward_dist < target_blk_cnt) {
+            throw ExtentOverlapError(ref, target, "(ext start is behind ref)");
+        }
+
+        blk_cnt = backward_dist - target_blk_cnt;
+        is_backwards = true;
+
+    } else {
+        throw ExtentOverlapError(ref, target, "(at same start)");
+    }
+
+    return {
+            .blk_cnt = blk_cnt,
+            .is_backwards = is_backwards,
+            .is_near = blk_cnt <= 0x1ff,
+    };
+}
+
+uint32_t Extent::calc_usable_space_size(uint8_t blk_sz_order) const {
+    const Extent& ext = *this;
+    if (ext.is_null()) {
+        return 0;
+    }
+
+    if (ext.is_suballoc()) {
+        return std::popcount(ext.blk_bitmap()) << (blk_sz_order - 4);
+    } else {
+        return ext.blk_cnt() << blk_sz_order;
+    }
 }
--- xoz/segm/segment.cpp
+++ xoz/segm/segment.cpp
@@ -1,6 +1,12 @@
 #include "xoz/segm/segment.h"
 
+#include <numeric>
 #include <ostream>
+
+#include "xoz/arch.h"
+#include "xoz/exceptions.h"
+#include "xoz/ext/extent.h"
+#include "xoz/ext/internal_defs.h"
 
 void PrintTo(const Segment& segm, std::ostream* out) {
     for (auto const& ext: segm.exts()) {
@@ -12,4 +18,95 @@ void PrintTo(const Segment& segm, std::ostream* out) {
 std::ostream& operator<<(std::ostream& out, const Segment& segm) {
     PrintTo(segm, &out);
     return out;
+}
+
+uint32_t Segment::calc_footprint_disk_size() const {
+    const Segment& segm = *this;
+
+    Extent prev(0, 0, false);
+
+    uint32_t sz = 0;
+    for (const auto& ext: segm.arr) {
+        // Ext header, always present
+        sz += sizeof(uint16_t);
+
+        Extent::blk_distance_t dist = Extent::distance_in_blks(prev, ext);
+        if (not dist.is_near) {
+            // Ext low blk nr bits, always present
+            // (ext is not an inline and it is not near)
+            sz += sizeof(uint16_t);
+        }
+
+        // blk_cnt is present only if
+        //   - OR is_suballoc (blk_cnt is a bitmap)
+        //   - OR ext.blk_nr is greater than EXT_SMALLCNT_MAX
+        //     (it cannot be representable by 4 bits)
+        if (ext.is_suballoc() or ext.blk_cnt() > EXT_SMALLCNT_MAX or ext.blk_cnt() == 0) {
+            sz += sizeof(uint16_t);
+        }
+
+        prev = ext;
+    }
+
+    if (segm.inline_present) {
+        // Ext header, always present
+        sz += sizeof(uint16_t);
+
+        segm.fail_if_bad_inline_sz();
+
+        // No blk_nr or blk_cnt are present in an inline
+        // After the header the uint8_t raw follows
+        //
+        // Note: the cast from size_t to uint16_t should be
+        // safe because if the size of the raw cannot be represented
+        // by uint16_t, fail_if_bad_inline_sz() should had failed
+        // before
+        uint16_t inline_sz = uint16_t(segm.raw.size());
+
+        // If size is odd, raw's last byte was saved in the ext header
+        // so the remaining data is size-1
+        if (inline_sz % 2 == 1) {
+            inline_sz -= 1;
+        }
+
+        sz += inline_sz;
+    }
+
+    return sz;
+}
+
+
+uint32_t Segment::calc_usable_space_size(uint8_t blk_sz_order) const {
+    const Segment& segm = *this;
+
+    uint32_t sz = std::accumulate(
+            segm.arr.cbegin(), segm.arr.cend(), 0,
+            [&blk_sz_order](uint32_t sz, const Extent& ext) { return sz + ext.calc_usable_space_size(blk_sz_order); });
+
+    if (segm.inline_present) {
+        segm.fail_if_bad_inline_sz();
+
+        // Note: the cast from size_t to uint16_t should be
+        // safe because if the size of the raw cannot be represented
+        // by uint16_t, fail_if_bad_inline_sz() should had failed
+        // before
+        uint16_t inline_sz = uint16_t(segm.raw.size());
+
+        // Note: calc_usable_space_size means how many bytes are allocated
+        // for user data so we register all the inline data as such
+        // (not matter if the size is an even or an odd number)
+        sz += inline_sz;
+    }
+
+    return sz;
+}
+
+uint32_t Segment::estimate_on_avg_internal_frag_sz(uint8_t blk_sz_order) const {
+    if (subblk_cnt() > 0) {
+        return 1 << (blk_sz_order - Extent::SUBBLK_SIZE_ORDER - 1);
+    } else if (full_blk_cnt() > 0) {
+        return 1 << (blk_sz_order - 1);
+    } else {
+        return 0;
+    }
 }
