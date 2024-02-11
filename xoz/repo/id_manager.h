643a518386d63e9917af70990c3195a2f5fb1384
Martin Di Paola <martinp.dipaola@gmail.com>
2024-02-10 23:16:48 -0300

    repo: request persistent ids

--- xoz/repo/id_manager.h
+++ xoz/repo/id_manager.h
@@ -9,6 +9,15 @@ public:
     IDManager() { reset(); }
 
     uint32_t request_temporal_id() { return next_temporal_id++; }
+    uint32_t request_persistent_id() {
+        uint32_t id = 1;
+        if (persistent_ids.size() > 0) {
+            id = (*persistent_ids.rbegin()) + 1;
+        }
+
+        register_persistent_id(id);
+        return id;
+    }
 
     // This makes sense only very special cases or for testing.
     void reset(uint32_t init = 0x80000000) {
