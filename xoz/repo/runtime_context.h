3c9dfd99158764808519f00e2fe6648487a7e893
Martin Di Paola <martinp.dipaola@gmail.com>
2024-07-14 21:43:03 -0300

    repo: make RuntimeContext a class not a struct (nitpick)

--- xoz/repo/runtime_context.h
+++ xoz/repo/runtime_context.h
@@ -7,7 +7,8 @@
 #include "xoz/dsc/descriptor_mapping.h"
 #include "xoz/repo/id_manager.h"
 
-struct RuntimeContext: public IDManager, public DescriptorMapping {
+class RuntimeContext: public IDManager, public DescriptorMapping {
+public:
     explicit RuntimeContext(const DescriptorMapping& dmap): IDManager(), DescriptorMapping(dmap) {}
     explicit RuntimeContext(const std::map<uint16_t, descriptor_create_fn>& descriptors_map,
                             bool override_reserved = false):
