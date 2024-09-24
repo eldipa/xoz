7ed6cde86db1f2e1fbb25ef9da43b32261aa38cb
Martin Di Paola <martinp.dipaola@gmail.com>
2024-09-24 20:10:21 -0300

    mem: print filename only

--- xoz/mem/asserts.cpp
+++ xoz/mem/asserts.cpp
@@ -2,10 +2,12 @@
 
 #ifndef NDEBUG
 #include <cstdlib>
+#include <filesystem>
 
 void xoz::internals::print_error(const char* msg, const char* cond_str, const char* file, unsigned int line,
                                  const char* func_str) {
-    fprintf(stderr, "%s at line %u - %s: %s (%s)\n", file, line, func_str, msg, cond_str);
+    auto fname = std::filesystem::path(file).filename();
+    fprintf(stderr, "%s at line %u - %s: (%s) failed\n-> %s\n", fname.c_str(), line, func_str, cond_str, msg);
 }
 
 void xoz::internals::abort_execution() { std::abort(); }
