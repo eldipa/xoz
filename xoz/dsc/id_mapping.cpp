0ce6325f278e447e66bc663ae1a1ff63774d1ee5
Martin Di Paola <martinp.dipaola@gmail.com>
2024-11-03 17:25:26 -0300

    dsc: id mapping descriptor (support index)

---
+++ xoz/dsc/id_mapping.cpp
@@ -0,0 +1,92 @@
+#include "xoz/dsc/id_mapping.h"
+
+namespace xoz {
+IDMappingDescriptor::IDMappingDescriptor(const struct Descriptor::header_t& hdr, BlockArray& cblkarr):
+        Descriptor(hdr, cblkarr), num_entries(0), content_sz(0) {}
+
+IDMappingDescriptor::IDMappingDescriptor(BlockArray& cblkarr):
+        Descriptor(IDMappingDescriptor::TYPE, cblkarr), num_entries(0), content_sz(0) {}
+
+void IDMappingDescriptor::read_struct_specifics_from(IOBase& io) { num_entries = io.read_u32_from_le(); }
+
+void IDMappingDescriptor::write_struct_specifics_into(IOBase& io) { io.write_u32_to_le(num_entries); }
+
+void IDMappingDescriptor::update_sizes(uint64_t& isize, uint64_t& csize) {
+    isize = sizeof(uint32_t);  // num entries
+    csize = content_sz;
+}
+
+std::unique_ptr<Descriptor> IDMappingDescriptor::create(const struct Descriptor::header_t& hdr, BlockArray& cblkarr,
+                                                        [[maybe_unused]] RuntimeContext& rctx) {
+    return std::unique_ptr<IDMappingDescriptor>(new IDMappingDescriptor(hdr, cblkarr));
+}
+
+uint32_t IDMappingDescriptor::calculate_store_mapping_size(const std::map<std::string, uint32_t>& id_by_name) const {
+    uint32_t sz = 0;
+    for (auto const& [name, id]: id_by_name) {
+        fail_if_bad_values(id, name);
+        sz += sizeof(id);
+        sz += sizeof(uint8_t);  // name length
+        sz += assert_u8(name.size());
+    }
+
+    return sz;
+}
+
+void IDMappingDescriptor::store(const std::map<std::string, uint32_t>& id_by_name) {
+    content_sz = calculate_store_mapping_size(id_by_name);
+    resize_content(content_sz);
+
+    // Note: sanity checks were made in calculate_store_mapping_size()
+    // No need to repeat them here.
+    auto io = get_content_io();
+    for (auto const& [name, id]: id_by_name) {
+        io.write_u32_to_le(id);
+        io.write_u8_to_le(assert_u8(name.size()));
+        io.writeall(name.data(), assert_u8(name.size()));
+    }
+}
+
+std::map<std::string, uint32_t> IDMappingDescriptor::load() {
+    std::map<std::string, uint32_t> id_by_name;
+
+    char buf[256];
+    auto io = get_content_io();
+    for (unsigned i = 0; i < num_entries; ++i) {
+        uint32_t id = io.read_u32_from_le();
+        uint8_t len = io.read_u8_from_le();
+
+        xoz_assert("", len <= sizeof(buf));
+
+        io.readall(buf, len);
+        std::string name(buf, len);
+
+        fail_if_bad_values(id, name);
+        id_by_name[name] = id;
+    }
+
+    return id_by_name;
+}
+
+void IDMappingDescriptor::fail_if_bad_values(uint32_t id, const std::string& name) const {
+    if (id == 0) {
+        throw std::runtime_error("Descriptor id '0' is not valid.");
+    }
+
+    if (id & 0x80000000) {
+        throw std::runtime_error("Descriptor id exceeds 2^31.");
+    }
+
+    if (name.size() > 255) {
+        throw std::runtime_error("Name for the descriptor is too large.");
+    }
+
+    if (name.size() == 0) {
+        throw std::runtime_error("Name for the descriptor cannot be empty.");
+    }
+
+    if (name == "/" or name == "." or name == "..") {
+        throw std::runtime_error("Name for the descriptor is reserved.");
+    }
+}
+}  // namespace xoz
---
+++ xoz/dsc/id_mapping.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <cstdint>
+#include <map>
+#include <memory>
+#include <string>
+
+#include "xoz/dsc/descriptor.h"
+#include "xoz/io/iobase.h"
+
+namespace xoz {
+
+class IDMappingDescriptor: public Descriptor {
+public:
+    constexpr static uint16_t TYPE = 0x09e0;
+
+    static std::unique_ptr<Descriptor> create(const struct Descriptor::header_t& hdr, BlockArray& cblkarr,
+                                              RuntimeContext& rctx);
+
+    /*
+     * Store/load the entire mapping.
+     * */
+    void store(const std::map<std::string, uint32_t>& id_by_name);
+    std::map<std::string, uint32_t> load();
+
+private:
+    IDMappingDescriptor(const struct Descriptor::header_t& hdr, BlockArray& cblkarr);
+    explicit IDMappingDescriptor(BlockArray& cblkarr);
+
+private:
+    void fail_if_bad_values(uint32_t id, const std::string& name) const;
+    uint32_t calculate_store_mapping_size(const std::map<std::string, uint32_t>& id_by_name) const;
+
+    uint32_t num_entries;
+    uint32_t content_sz;
+
+private:
+    void read_struct_specifics_from(IOBase& io) override;
+    void write_struct_specifics_into(IOBase& io) override;
+    void update_sizes(uint64_t& isize, uint64_t& csize) override;
+};
+}  // namespace xoz
