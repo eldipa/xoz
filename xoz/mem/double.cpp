1f63a9f0540b17f553b1125cb727d55112066354
Martin Di Paola <martinp.dipaola@gmail.com>
2025-02-27 00:46:51 -0300

    mem: enable double float to le and back casts

--- xoz/mem/double.cpp
+++ xoz/mem/double.cpp
@@ -14,14 +14,10 @@ uint32_t single_float_to_le(double num) { return internals::impl_double_to_le<ui
 
 double single_float_from_le(uint32_t num) { return internals::impl_double_from_le<uint32_t, 8>(num); }
 
-/*
-uint64_t double_float_to_le(double num) {
-    return internals::impl_double_to_le<uint64_t, 11>(num);
-}
 
-double double_float_from_le(uint64_t num) {
-    return internals::impl_double_from_le<uint64_t, 11>(num);
-}
-*/
+uint64_t double_float_to_le(double num) { return internals::impl_double_to_le<uint64_t, 11>(num); }
+
+double double_float_from_le(uint64_t num) { return internals::impl_double_from_le<uint64_t, 11>(num); }
+
 
 }  // namespace xoz
